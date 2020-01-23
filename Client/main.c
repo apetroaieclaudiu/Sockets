9715d2b5fe1bff43e62a53ef3bf302e1a4140459
apetroaieclaudiu <apetroaie.claudiu@gmail.com>
2020-01-23 16:21:46 +0200

    Client-Server working for one request at the time

--- Client/main.c
+++ Client/main.c
@@ -3,40 +3,85 @@
 #include <arpa/inet.h> 
 #include <unistd.h> 
 #include <string.h> 
+#include <sys/types.h>
+#include <stdlib.h>
+#include <errno.h>
 
 #define PORT 8080 
 #define MAX_BUFFER_SIZE 1024
-   
+
 int main(int argc, char const *argv[]) 
 { 
-    int sock = 0, valread; 
-    struct sockaddr_in serv_addr; 
-    char *hello = "Hello from client"; 
-    char buffer[1024] = {0}; 
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
-    { 
-        printf("\n Socket creation error \n"); 
-        return -1; 
-    } 
-   
-    serv_addr.sin_family = AF_INET; 
-    serv_addr.sin_port = htons(PORT); 
-       
-    // Convert IPv4 and IPv6 addresses from text to binary form 
-    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)  
-    { 
-        printf("\nInvalid address/ Address not supported \n"); 
-        return -1; 
-    } 
-   
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
-    { 
-        printf("\nConnection Failed \n"); 
-        return -1; 
-    } 
-    send(sock , hello , strlen(hello) , 0 ); 
-    printf("Hello message sent\n"); 
-    valread = read( sock , buffer, 1024); 
-    printf("%s\n",buffer ); 
-    return 0; 
+	int sock = 0, valread; 
+	struct sockaddr_in serv_addr; 
+	char buffer[1024] = {0};
+	FILE *received_file;
+	ssize_t len;
+	char received_length[MAX_BUFFER_SIZE];
+	int file_size;
+	int remain_data = 0;
+	char aux[256];
+
+	if (argc != 2) {
+		printf("\nInvalid number of arguments\n");
+		return -1;
+	}
+
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
+	{ 
+		printf("\n Socket creation error \n"); 
+		return -1; 
+	} 
+
+	serv_addr.sin_family = AF_INET; 
+	serv_addr.sin_port = htons(PORT); 
+
+	// Convert IPv4 and IPv6 addresses from text to binary form 
+	if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)  
+	{ 
+		printf("\nInvalid address/ Address not supported \n"); 
+		return -1; 
+	} 
+
+
+	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
+	{ 
+		printf("\nConnection Failed \n"); 
+		return -1; 
+	} 
+	send(sock, argv[1], strlen(argv[1]), 0);
+	recv(sock, buffer, MAX_BUFFER_SIZE, 0);
+	printf("\nServer message: %s\n", buffer);
+	file_size = atoi(buffer);
+	fprintf(stdout, "\nFile size : %d\n", file_size);
+
+	received_file = fopen("requestedFile", "w");
+	if (received_file == NULL)
+	{
+		fprintf(stderr, "Failed to open file foo --> %s\n", strerror(errno));
+
+		exit(EXIT_FAILURE);
+	}
+
+	remain_data = file_size;
+ 
+	while ((remain_data > 0) && ((len = recv(sock, buffer, MAX_BUFFER_SIZE, 0)) > 0))
+	{
+		recv(sock, received_length, MAX_BUFFER_SIZE, 0);
+		sprintf(aux, "%d", len);
+		if (strcmp(aux, received_length) == 0) {
+			fwrite(buffer, sizeof(char), len, received_file);
+			remain_data -= len;
+			fprintf(stdout, "Receive %ld bytes and we hope :- %d bytes\n", len, remain_data);
+		}
+		else {
+			fprintf(stdout, "Missing some data, Retrying....\n");
+		}
+		send(sock, aux, MAX_BUFFER_SIZE, 0);
+	}
+	fclose(received_file);
+
+	close(sock);
+
+	return 0; 
 } 
--- Server/main.c
+++ Server/main.c
@@ -8,6 +8,10 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <sys/time.h>
+#include <sys/sendfile.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
 
 #define MAX_BUFFER_SIZE 1024
 #define TRUE   1
@@ -16,153 +20,179 @@
 
 int main(int argc , char *argv[])
 {
-    char source[MAX_BUFFER_SIZE + 1];
-    int opt = TRUE;
-    int master_socket , addrlen , new_socket , client_socket[30] ,
-          max_clients = 30 , activity, i , valread , sd;
-    int max_sd;
-    struct sockaddr_in address;
+	char source[MAX_BUFFER_SIZE + 1], file_size[MAX_BUFFER_SIZE];
+	int opt = TRUE;
+	int master_socket , addrlen , new_socket , client_socket[30] ,
+	    max_clients = 30 , activity, i , valread , sd;
+	int max_sd;
+	struct sockaddr_in address;
+	struct stat file_stat;
+	char buffer[MAX_BUFFER_SIZE];
+	int offset;
+	int remain_data, sent_bytes;
+	fd_set readfds;
 
-    char buffer[MAX_BUFFER_SIZE];
-    fd_set readfds;
+	char *message = "Welcome aboard! Connection succesfull. \r\n";
 
-    char *message = "Welcome aboard! Connection succesfull. \r\n";
+	for (i = 0; i < max_clients; i++)
+	{
+		client_socket[i] = 0;
+	}
 
-    for (i = 0; i < max_clients; i++)
-    {
-        client_socket[i] = 0;
-    }
+	if( (master_socket = socket(AF_INET , SOCK_STREAM , 0)) == 0)
+	{
+		perror("socket failed");
+		exit(EXIT_FAILURE);
+	}
 
-    if( (master_socket = socket(AF_INET , SOCK_STREAM , 0)) == 0)
-    {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
+	if( setsockopt(master_socket, SOL_SOCKET, SO_REUSEADDR, (char *)&opt,
+				sizeof(opt)) < 0 )
+	{
+		perror("setsockopt");
+		exit(EXIT_FAILURE);
+	}
 
-    if( setsockopt(master_socket, SOL_SOCKET, SO_REUSEADDR, (char *)&opt,
-          sizeof(opt)) < 0 )
-    {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons( PORT );
 
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
+	if (bind(master_socket, (struct sockaddr *)&address, sizeof(address))<0)
+	{
+		perror("bind failed");
+		exit(EXIT_FAILURE);
+	}
+	printf("Listener on port %d \n", PORT);
 
-    if (bind(master_socket, (struct sockaddr *)&address, sizeof(address))<0)
-    {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
-    printf("Listener on port %d \n", PORT);
+	if (listen(master_socket, 3) < 0)
+	{
+		perror("listen");
+		exit(EXIT_FAILURE);
+	}
 
-    if (listen(master_socket, 3) < 0)
-    {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
+	addrlen = sizeof(address);
+	puts("Waiting for connections ...");
 
-    addrlen = sizeof(address);
-    puts("Waiting for connections ...");
+	while(TRUE)
+	{
+		FD_ZERO(&readfds);
 
-    while(TRUE)
-    {
-        FD_ZERO(&readfds);
+		FD_SET(master_socket, &readfds);
+		max_sd = master_socket;
 
-        FD_SET(master_socket, &readfds);
-        max_sd = master_socket;
+		for ( i = 0 ; i < max_clients ; i++)
+		{
+			sd = client_socket[i];
 
-        for ( i = 0 ; i < max_clients ; i++)
-        {
-            sd = client_socket[i];
+			if(sd > 0)
+				FD_SET( sd , &readfds);
 
-            if(sd > 0)
-                FD_SET( sd , &readfds);
+			if(sd > max_sd)
+				max_sd = sd;
+		}
 
-            if(sd > max_sd)
-                max_sd = sd;
-        }
+		activity = select( max_sd + 1 , &readfds , NULL , NULL , NULL);
 
-        activity = select( max_sd + 1 , &readfds , NULL , NULL , NULL);
+		if ((activity < 0) && (errno!=EINTR))
+		{
+			printf("select error");
+		}
 
-        if ((activity < 0) && (errno!=EINTR))
-        {
-            printf("select error");
-        }
+		if (FD_ISSET(master_socket, &readfds))
+		{
+			if ((new_socket = accept(master_socket,
+							(struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
+			{
+				perror("accept");
+				exit(EXIT_FAILURE);
+			}
 
-        if (FD_ISSET(master_socket, &readfds))
-        {
-            if ((new_socket = accept(master_socket,
-                    (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
-            {
-                perror("accept");
-                exit(EXIT_FAILURE);
-            }
+			printf("New connection , socket fd is %d , ip is : %s , port : %d\n"
+					,new_socket, inet_ntoa(address.sin_addr) , ntohs
+					(address.sin_port));
 
-            printf("New connection , socket fd is %d , ip is : %s , port : %d\n"
-              ,new_socket, inet_ntoa(address.sin_addr) , ntohs
-                  (address.sin_port));
+			for (i = 0; i < max_clients; i++)
+			{
+				if( client_socket[i] == 0 )
+				{
+					client_socket[i] = new_socket;
+					printf("Adding to list of sockets as %d\n" , i);
 
-            if( send(new_socket, message, strlen(message), 0) != strlen(message) )
-            {
-                perror("send");
-            }
+					break;
+				}
+			}
+		}
 
-            puts("Welcome message sent successfully");
+		for (i = 0; i < max_clients; i++)
+		{
+			sd = client_socket[i];
 
-            for (i = 0; i < max_clients; i++)
-            {
-                if( client_socket[i] == 0 )
-                {
-                    client_socket[i] = new_socket;
-                    printf("Adding to list of sockets as %d\n" , i);
+			if (FD_ISSET( sd , &readfds))
+			{
+				if ((valread = read( sd , buffer, 1024)) == 0)
+				{
+					getpeername(sd , (struct sockaddr*)&address , \
+							(socklen_t*)&addrlen);
+					printf("Host disconnected , ip %s , port %d \n" ,
+							inet_ntoa(address.sin_addr) , ntohs(address.sin_port));
 
-                    break;
-                }
-            }
-        }
+					close( sd );
+					client_socket[i] = 0;
+				}
 
-        for (i = 0; i < max_clients; i++)
-        {
-            sd = client_socket[i];
+				else
+				{
+					buffer[valread] = '\0';
+					int fp = open(buffer, O_RDONLY);
+					int len;
+					size_t newLen;
+					char length[10];
+					int old_offset;
 
-            if (FD_ISSET( sd , &readfds))
-            {
-                if ((valread = read( sd , buffer, 1024)) == 0)
-                {
-                    getpeername(sd , (struct sockaddr*)&address , \
-                        (socklen_t*)&addrlen);
-                    printf("Host disconnected , ip %s , port %d \n" ,
-                          inet_ntoa(address.sin_addr) , ntohs(address.sin_port));
+					if (fp==-1) {
+						fputs ("File error",stderr); 
+						break;
+					}
 
-                    close( sd );
-                    client_socket[i] = 0;
-                }
+					if (fstat(fp, &file_stat) < 0) {
+						fprintf(stderr, "Error fstat --> %s", strerror(errno));
+						exit(EXIT_FAILURE);
+					}
+					offset = 0;
+					old_offset = 0;
+					remain_data = file_stat.st_size;
+					sprintf(file_size, "%d", file_stat.st_size);
+					printf("The size of the file is: %s\n", file_size);
+					/* Sending file size */
+					len = send(sd, file_size, sizeof(file_size), 0);
+					if (len < 0)
+					{
+						fprintf(stderr, "Error on sending greetings --> %s", strerror(errno));
 
-                else
-                {
-                    buffer[valread] = '\0';
-                    FILE *fp = fopen(buffer, "r");
-                    size_t newLen;
-                    strcpy(source, "../file1.txt");
-                    newLen = fread(source, sizeof(char), MAX_BUFFER_SIZE - 1, fp);
-                    if (fp != NULL) {
-                        while(newLen > 0) {
-                            if ( ferror( fp ) != 0 ) {
-                                strcpy(source, "Error reading the file");
-                            } else {
-                                source[newLen++] = '\0';
-                            }
-                            send(sd , source , strlen(source) , 0 );
-                            newLen = fread(source, sizeof(char), MAX_BUFFER_SIZE - 1, fp);
-                        }
-                        fclose(fp);
-                    }
-                }
-            }
-        }
-    }
+						exit(EXIT_FAILURE);
+					}
+					while (((sent_bytes = sendfile(sd, fp, &offset, MAX_BUFFER_SIZE)) > 0) && (remain_data > 0))
+					{
+						sprintf(length, "%d", sent_bytes);
+						send(sd,  length, MAX_BUFFER_SIZE, 0);
+						recv(sd, file_size, MAX_BUFFER_SIZE, 0);
+						printf("\nRec:%s\n\nSend:%s\n", file_size, length); 
+						if (strcmp(file_size, length) == 0) {
+							fprintf(stdout, "1. Server sent %d bytes from file's data, offset is now : %ld and remaining data = %d\n", sent_bytes, offset, remain_data);
+							remain_data -= sent_bytes;
+							fprintf(stdout, "2. Server sent %d bytes from file's data, offset is now : %ld and remaining data = %d\n", sent_bytes, offset, remain_data);
+						}
+						else {
+							fprintf(stdout, "Incomplete package. Retrying data transmission");
+							offset = old_offset;
+						}
+						old_offset = offset;
 
-    return 0;
+					}
+				}
+			}
+		}
+	}
+
+
+	return 0;
 }
